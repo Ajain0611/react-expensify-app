2e73ae1e12ebf6cd2d4f8fd49ac905cd82568ca8
Anurag Jain (INFOSYS LIMITED) <v-aja@microsoft.com>
2020-04-06 01:55:53 +0530

    set test database environment

---
+++ src/firebase/firebase.js
@@ -0,0 +1,46 @@
+import * as firebase from 'firebase';
+import { PureComponent } from 'react';
+import moment from 'moment';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+
+firebase.initializeApp(config);
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+//         console.log(expenses);
+//     })
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').push({
+//     description: 'Credit Card',
+//     note: '',
+//     amount: 4500,
+//     createdAt: moment(0).add(4, 'days').valueOf()
+// })
